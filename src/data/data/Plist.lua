56202763ecb518d48b0483adf44dc5fd819ab818
chenhlb <chenhlb8055@gmail.com>
2017-07-12 15:58:22 +0800

    rm

--- src/data/data/Plist.lua
+++
@@ -1,8 +0,0 @@
-local PlistConfig = {
-	{
-		id = 2000001,
-		path = "client/map_build/wood.plist",
-	},
-
-}
-return PlistConfig
--- src/data/data/Sprite.lua
+++
@@ -1,8 +0,0 @@
-local SpriteConfig = {
-	{
-		id = 1001,
-		path = "client/activity/activity_1001.png",
-	},
-
-}
-return SpriteConfig
